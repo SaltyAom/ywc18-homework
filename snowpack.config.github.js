d7fdb90445179bd6c79838951c391ea15f3b6c41
SaltyAom <aomkirby123@gmail.com>
2020-11-19 23:44:34 +0700

    :blue_book: feat - experimental: config for github page

--- snowpack.config.github.js
+++ snowpack.config.github.js
@@ -1,28 +1,28 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    public: '/ywc18-homework',
-    src: '/ywc18-homework/_dist_',
-    // src: '/_dist_',
-  },
-  plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
+    mount: {
+        web_modules: '/ywc18-homework/web_modules',
+        public: '/ywc18-homework',
+        src: '/ywc18-homework/_dist_',
+        // src: '/_dist_',
+    },
+    plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
+    install: [
+        /* ... */
+    ],
+    installOptions: {
+        /* ... */
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {
+      webModulesUrl: "ywc18-homework/web_modules"
+    },
+    proxy: {
+        /* ... */
+    },
 
-  alias: {
-    "web_modules": "ywc18-homework/web_modules"
-  },
-};
+    alias: {
+    },
+}
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: '/,
+    public: '/',
     src: '/_dist_',
   },
   plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
