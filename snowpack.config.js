158b0479422a2badd4873538b0455c787f00c02c
SaltyAom <aomkirby123@gmail.com>
2020-11-19 23:16:57 +0700

    :blue_book: feat - experimental: prefix for github page

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: '/',
+    public: '/ywc18-homework',
     src: '/ywc18-homework/_dist_',
     // src: '/_dist_',
   },
